5fdb03776c3949b8aca66d7a7feaa3692d42c147
IamSynthetiC81 <66788358+IamSynthetiC81@users.noreply.github.com>
2022-11-29 18:16:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,244 +1,181 @@
 import json
 import random
-import sys
 from datetime import datetime
 
-# Global Path Variable
-Path = "tweetdhead300000.json"
+import logging
+import sys
 
-# Profiling : Save length of tweets on buffer instead of calling len().
-TweetsSize = int(0)
+#START OF LOGGING
+logger = logging.getLogger("Log_Information")
+#Required whole pathing for the file to work (CHANGE)
+logging.basicConfig(filename='C:\\Users\\Helioc\\Desktop\\Sxoli\\ERGALEIA\\TweetyTerminal-main\\logme.txt', filemode='w', format= '%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO, force= True)
+#END OF LOGGING
 
-def loadTweets(path=Path) -> list:
-    """
-    Load tweets from the specified path and return them
-    :param path: Path to the json file containing the tweets
-    :return:  Returns a list of tweets
-    """
-    array = []
-    with open(path, "r") as f:
+def loadTweets(path):
+    tweets=[]
+    with open(path,"r") as f:
         lines = f.readlines()
         for line in lines:
-            if line == "\n":
-                continue
             tweet = json.loads(line)
-            array.append(tweet)
+            tweets.append(tweet)
         f.close()
+    return tweets
 
-    global TweetsSize
-    TweetsSize = len(array)
-
-    return array
-
-
-tweetArray = loadTweets()               # Global Variable containing tweets
-
-
-def deleteTweet(idx, tweets=None) -> None:
-    """
-    Delete the tweet in the specified index
-    :param idx: Index of the tweet to be deleted
-    :param tweets: List of tweets. By default, '~tweetArray' is used.
-    """
-    if tweets is None:
-        tweets = tweetArray
+def deleteTweet(idx,tweets):
     del tweets[idx]
 
-    # Profiling : Decrement length.
-    global TweetsSize
-    TweetsSize -= 1
+def readTweet(idx,tweets):
+    print("Tweet at index "+str(idx)+": "+str(tweets[idx]['text'])+" created at: "+str(tweets[idx]['created_at']))
 
+def createTweet(current,tweets):
+    updateTweet(len(tweets),tweets)
 
-def readTweet(idx, tweets=None) -> None:
-    """
-    Prints out the tweet at the specified index.
-    :param idx: Index of the tweet
-    :param tweets: List of tweets. By default, '~tweetArray' is used.
-    """
-    if tweets is None:
-        tweets = tweetArray
-
-    print("Tweet at index " + str(idx) + ": " + str(tweets[idx]['text']) + " created at: " + str(
-        tweets[idx]['created_at']))
-
-
-def createTweet(string=None, tweets=None) -> None:
-    """
-    Creates a new tweet and appends it to the list
-    :param string: Text of the new tweet. Can be ignored for console input
-    :param tweets: List of tweets. By default, '~tweetArray' is used.
-    """
-    if tweets is None:
-        tweets = tweetArray
-
+def updateTweet(current,tweets):
     date = datetime.now()
     date = str(date)
+    text = input("Please type your tweet:\n")
+    tweet = {'text':text,'created_at':date}
+    tweets.insert(current,tweet)
 
-    if string is None:
-        string = input("Please type your tweet:\n")
-
-    tweet = {'text': string, 'created_at': date}
-    tweets.append(tweet)
-
-    global TweetsSize
-    TweetsSize += 1
-
-
-def updateTweet(current, text, tweets=None) -> None:
-    """
-    Updates the indexed tweet with the specified text.
-    :param current: Index of the tweet to be altered.
-    :param text: String to be placed into the tweet
-    :param tweets: List of tweets. By default, '~tweetArray' is used.
-    """
-    if tweets is None:
-        tweets = tweetArray
-
-    date = datetime.now()
-    date = str(date)
-    tweet = {'text': text, 'created_at': date}
-
-    tweets[current] = tweet
-
-
-def saveToDisk(path="output.json", tweets=None) -> None:
-    """
-    Saves the specified list of tweets into the specified path
-    :param path: Name and path of the file to be created or saved on.
-    :param tweets: List of tweets. By default, '~tweetArray' is used.
-    """
-    if tweets is None:
-        tweets = tweetArray
-
-    with open(path, "w") as f:
+def saveToDisk(tweets):
+    #Required whole pathing for the file to work (CHANGE)
+    with open("C:\\Users\\Helioc\\Desktop\\Sxoli\\ERGALEIA\\TweetyTerminal-main\\output.json","w") as f:
+        tweet = ""
         final = ""
         for i in tweets:
             tweet = json.dumps(i)
-            final = final + "\n" + tweet
+            final =final+"\n"+tweet
         f.write(final)
     f.close()
 
-
-def game(tweets) -> None:
-    """
-    Prints a random tweet from the list
-    :param tweets: List of tweets. By default, '~tweetArray' is used.
-    """
+def game(tweets):
     random.seed(datetime.now())
-    idx = random.randrange(0, TweetsSize - 1)
-    print(tweets[idx]['text'])
+    idx = random.randrange(0,len(tweets)-1)
+    print(tweets[idx]['text']) 
 
-
-def printHelp() -> None :
-    """
-    Helper function to print the menu
-    """
-    print(
-        "======================================================\nc: Create new tweet\nr <tweet ID>: Read tweet with "
-        "specified ID\nu <tweet ID>: Update tweet with specified ID\nd: Delete current tweet\ng: Display random tweet "
-        "for the lulz\n$: Read last tweet\n-: Read one tweet down\n+: Read one tweet down\n=: Print current tweet\nq: "
-        "Quit\nw: Save to disk\nx: Exit and save\n======================================================")
-
+def printHelp():
+    print("======================================================\nc: Create new tweet\nr <tweet ID>: Read tweet with specified ID\nu <tweet ID>: Update tweet with specified ID\nd: Delete current tweet\ng: Display random tweet for the lulz\n$: Read last tweet\n-: Read one tweet down\n+: Read one tweet down\n=: Print current tweet\nq: Quit\nw: Save to disk\nx: Exit and save\n======================================================")
 
 def terminalUI():
     current = 0
+    #Required whole pathing for the file to work (CHANGE)
+    tweets = loadTweets("C:\\Users\\Helioc\\Desktop\\Sxoli\\ERGALEIA\\TweetyTerminal-main\\input.json")
     print("Welcome to the US political degeneracy cesspool(Twitter)\nType h for help...")
-    quit = False
-    a = ' '
-    while not quit:
-        a = input("--->")
+    quit = False 
+    a=' '
+    while quit==False:
+        a=input("--->")
         a = str(a)
         a = a.strip()
         a = a.split(" ")
-        quit, current = menu(current, tweetArray, a)
+        quit ,current = menu(current,tweets,a)
 
+def menu(current,tweets,a):
 
-def menu(current, tweets, a):
-    if a[0] == "d":  # Delete tweet
-        deleteTweet(current, tweets)
+        if a[0] == "d":
+            logger.info("User deleted a tweet. (d)")
+            deleteTweet(current,tweets)
 
-    elif a[0] == "h":  # Display Help (man sex)
-        printHelp()
+        elif a[0] == "h":
+            logger.info("User asked for help. (h)")
+            printHelp()
 
-    elif a[0] == "g":  # Display random tweet
-        game(tweets)
+        elif a[0] == "g":
+            logger.info("User played a game with random tweets. (g)")
+            game(tweets,)
 
-    elif a[0] == "q":  # Quit
-        return True, current
+        elif a[0] == "q":
+            logger.info("User quit Twitter Editor without save. (q)")    
+            return True, current
+                   
+        elif a[0] == "w":
+            logger.info("User overwrited tweet to disk. (w)") 
+            saveToDisk(tweets)
+            
 
-    elif a[0] == "w":  # Save To disk
-        saveToDisk(tweets)
+        elif a[0] == "x":
+            logger.info("User exited Twitter Editor with save. (x)") 
+            saveToDisk(tweets)           
+            return True, current
 
-    elif a[0] == "x":  # Exit and Save
-        saveToDisk(tweets)
-        return True, current
+        elif a[0] == "i":
+            logger.info("User printed the tweet list. (i)") 
+            print(len(tweets))
 
-    elif a[0] == "i":  # ???
-        # TODO : Fix this
-        print(len(tweets))
+        elif a[0] == "=":
+            logger.info("User printed current tweet ID. (=)") 
+            print("Current index: "+str(current))
 
-    elif a[0] == "=":  # Print Current Tweet
-        print("Current index: " + str(current))
-        #TODO : Fix this
+        elif a[0] == "$":
+            logger.info("User read the last tweet in the file. ($)")
+            current = len(tweets)-1
+            readTweet(current,tweets)
+            
 
-    elif a[0] == "$":  # Print Last Tweet
-        current = TweetsSize - 1
-        readTweet(current, tweets)
+        elif a[0] == "-":
+            if current==0:
+                print("You've reached the top")
+                return False, current 
+            current-=1
+            logger.info("User read one tweet up from their current tweet. (-)") 
+            readTweet(current,tweets)
+            
 
-    elif a[0] == "-":  # Print Previous Tweet
-        if current == 0:
-            print("You've reached the bottom")
-            return False, current
-        current -= 1
-        readTweet(current, tweets)
+        elif a[0] == "+":
+            if current==len(tweets)-1:
+                print("You've reached the bottom")
+                return False, current
+            current+=1
+            logger.info("User read one tweet down from their current tweet. (+)") 
+            readTweet(current,tweets)
+            
 
-    elif a[0] == "+":  # Print Next Tweet
-        if current == len(tweets) - 1:
-            print("You've reached the top")
-            return False, current
-        current += 1
-        readTweet(current, tweets)
+        elif a[0] == "c":
+            logger.info("User created a tweet. (c)") 
+            createTweet(current,tweets)
+            current = len(tweets)-1
 
-    elif a[0] == "c":  # Create a new Tweet
-        createTweet()
-        current = TweetsSize - 1
+        elif a[0] == "r":   
+            try:
+                a[1] = int(a[1])
+            except:
+                print("Incorrect arguments")
+                return False, current
 
-    elif a[0] == "r":  # Read Tweet with specified ID
-        try:
-            a[1] = int(a[1])
-        except:
-            print("Incorrect arguments")
-            return False, current
+            if a[1]<0 or a[1]>len(tweets):
+                print("Index out of range")
 
-        if a[1] < 0 or a[1] > TweetsSize:
-            print("Index out of range")
+            else:
+                current = a[1]
+                logger.info("User read a tweet. (r)") 
+                readTweet(current,tweets)                
+
+        elif a[0] == "u":   
+            try:
+                a[1] = int(a[1])
+            except:
+                print("Incorrect arguments")
+                return False,current
+
+            if a[1]<0 or a[1]>len(tweets):
+                print("Index out of range")
+
+            else:
+                current = a[1]
+                logger.info("User updated a tweet. (u)") 
+                updateTweet(current,tweets)
+                
 
         else:
-            current = a[1]
-            readTweet(current, tweets)
+            logger.info("User tried an unrecognised command...") 
+            print("Command not recognised...")
+        return False, current
+    
 
-    elif a[0] == "u":  # Update Tweet with specified ID
-        try:
-            a[1] = int(a[1])
-        except:
-            print("Incorrect arguments")
-            return False, current
+def main(argv): 
 
-        if a[1] < 0 or a[1] > TweetsSize:
-            print("Index out of range")
-
-        else:
-            current = a[1]
-            updateTweet(current, input("Please type your tweet:\n"))
-
-    else:
-        print("Command not recognised...")  # Wrong command nerd
-    return False, current
-
-
-def main(argv):
     terminalUI()
 
 
 if __name__ == "__main__":
-    sys.exit(main(sys.argv[1:]))
+    main(sys.argv[1:])
